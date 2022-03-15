351b0ef48eb5e0fd816af9f16308b79b97bddeff
Mihajlo <milojevicm374@gmail.com>
2022-03-15 16:41:22 +0100

    title bar buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import Window from "./components/window";
-import TitleBar from "./components/titleBar";
 
 function App() {
   return (
     <>
-      <Window width={{value: 50, unit: "vh"}} height={{value: 15, unit: "vh"}}
-      titleBar={<TitleBar/>}>
+      <Window width={{value: 50, unit: "vh"}} height={{value: 15, unit: "vh"}} show={true}>
         <p>Hello</p>
       </Window>
-      <Window width={{value: 400, unit: "px"}} height={{value: 300, unit: "px"}}
-        titleBar={<TitleBar/>}
-      >
-        <iframe title="yt" src="https://express-bus-booking.netlify.app/" style={{width: "100%", height: "100%"}}></iframe>
+      <Window width={{value: 400, unit: "px"}} height={{value: 300, unit: "px"}} show={true}>
+        <iframe title="iframe" src="https://express-bus-booking.netlify.app/" style={{width: "100%", height: "100%"}}></iframe>
       </Window>
     </>
   );
--- src/components/window.js
+++ src/components/window.js
@@ -2,18 +2,36 @@ import { useEffect, useState,  } from "react";
 import Draggable from "react-draggable";
 import { ResizableBox } from 'react-resizable';
 import useWindowDimensions from "../utils/useWindowDimensions";
+import TitleBar from "./titleBar";
 
 import "./window.css"
 
-export default function Window({children, width, height, titleBar}) {
-    const [dimensions, setDimension] = useState({ width: width.value, height: height.value })
+export default function Window({children, width, height, titleBar, show}) {
+    const [dimensions, setDimension] = useState({ width: width.value, height: height.value, max: false })
     const {width: windowWidth, height: windowHeight} = useWindowDimensions();
-
-
-    useEffect(() => {
+    const [visible, setVisible] = useState(show);
+    const minimaze = () => {
+        setVisible(false)
+    }
+    const maximaze = () => {
+        if(dimensions.max)
+            calculateDimensions(width, height, false)
+        else
+            calculateDimensions(
+                { value: 100,unit: "vw"},
+                { value: 100,unit: "vh"},
+                true
+            )
+    }
+    const close = () => {
+        setVisible(false)        
+    }
+    const titleBarOptions = {maximaze, minimaze, close};
+    const calculateDimensions = (width, height, max) => {
         const newDimensions = {
             width: width.value,
             height: height.value,
+            max
         }
         switch (width.unit) {
             case "vw":
@@ -48,6 +66,10 @@ export default function Window({children, width, height, titleBar}) {
                 break;
         }
         setDimension(newDimensions);
+    }
+
+    useEffect(() => {
+        calculateDimensions(width, height, false);
     }, [width, height, windowWidth, windowHeight])
 
     const onDragStart = (e) => {
@@ -59,28 +81,33 @@ export default function Window({children, width, height, titleBar}) {
       }
 
     return (
-        <Draggable
-            handle=".title-bar"
-            onMouseDown={onDragStart}
-        >
-            <ResizableBox
-                className="resizable-box window"
-                width={dimensions.width}
-                height={dimensions.height}
-                handle={(h, ref) => <span className={`custom-handle`} ref={ref} />}
-                handleSize={[8, 8]}
-                resizeHandles={['se']}
-            >
-                <>
-                    <div style={{width: "100%"}}>
-                        {titleBar}
-                    </div>
-                    <div style={{width: "100%", objectFit: "cover"}}>
-                        {children}
-                    </div>
-                </>
-            </ResizableBox>
-        </Draggable>
+        <>
+            {
+                visible &&
+                <Draggable
+                handle=".title-bar"
+                onMouseDown={onDragStart}
+                >
+                    <ResizableBox
+                        className="resizable-box window"
+                        width={dimensions.width}
+                        height={dimensions.height}
+                        handle={(h, ref) => <span className={`custom-handle`} ref={ref} />}
+                        handleSize={[8, 8]}
+                        resizeHandles={['se']}
+                    >
+                        <>
+                            <div style={{width: "100%"}}>
+                                <TitleBar {...titleBarOptions}/>
+                            </div>
+                            <div style={{width: "100%", objectFit: "cover"}}>
+                                {children}
+                            </div>
+                        </>
+                    </ResizableBox>
+                </Draggable>
+            }
+        </>
     )
 }
       
